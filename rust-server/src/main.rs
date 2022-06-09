f17b15ba1208c6110d2a42d315f56bd1a556ca84
Nertsal <sasha.kudasov04@gmail.com>
2022-06-09 14:42:15 +0300

    [Rust] Setup rocket server

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,10 +1,19 @@
 mod database;
 mod game;
 
-use database::{psql::*, DBAccessor};
+// use database::{psql::*, DBAccessor};
 
-fn main() -> PsqlResult<()> {
-    let db = PsqlAccess::new()?;
+#[rocket::launch]
+fn launch() -> _ {
+    let config = rocket::Config {
+        log_level: rocket::log::LogLevel::Debug,
+        address: std::net::Ipv4Addr::new(127, 0, 0, 1).into(),
+        port: 8000,
+        ..Default::default()
+    };
+    rocket(config)
+}
 
-    Ok(())
+fn rocket(config: rocket::Config) -> rocket::Rocket<rocket::Build> {
+    rocket::custom(config).mount("/", rocket::routes![])
 }
