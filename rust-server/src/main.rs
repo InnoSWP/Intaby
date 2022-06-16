7099722dc0790ac477173e71b04339d85be56145
ADari-Ka <ADari.Ka@yandex.ru>
2022-06-16 16:22:26 +0300

    python docker fix

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -8,8 +8,8 @@ mod tests;
 async fn launch() -> _ {
     let config = rocket::Config {
         log_level: rocket::log::LogLevel::Debug,
-        address: std::net::Ipv4Addr::new(127, 0, 0, 1).into(),
-        port: 8000,
+        address: std::net::Ipv4Addr::new(0, 0, 0, 0).into(),
+        port: 8080,
         ..Default::default()
     };
     server::rocket(config, "postgresql://test:test@postgres_db:5432/test").await
--- web-app/app_start.py
+++ web-app/app_start.py
@@ -1,4 +1,4 @@
 from entrypoints.flask_app import app
 
 if __name__ == "__main__":
-    app.run(port=8888, host='0.0.0.0')
+    app.run()
