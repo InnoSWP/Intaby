002c80dee3b25c06d0618665987db1a6cbba0318
rusin <rusin.senya@mail.ru>
2022-06-24 18:01:29 +0300

    Script v 1.0

--- web-app/static/scripts/addAnswer.js
+++ web-app/static/scripts/addAnswer.js
@@ -5,7 +5,7 @@ addButton[0].addEventListener('click', function addAnswer() {
     clicks += 1
     if (clicks <= 6) {
         var newAnswer = document.createElement('textarea')
-        newAnswer.classList.add("Answer_type", "col-12", "text-white", "px-2", "py-2", "mb-2", "mb-lg-4", "mb-2", "mb-lg-4", "rounded")
+        newAnswer.classList.add("Answer_type", "col-12", "text-white", "px-2", "py-2", "mb-2", "mb-lg-4", "rounded")
         newAnswer.placeholder = "Answer " + clicks
         newAnswer.style.backgroundColor = "black"
         var wrapper = document.getElementsByClassName("Answer_add_wrapper")
@@ -13,13 +13,13 @@ addButton[0].addEventListener('click', function addAnswer() {
             newAnswer.style.backgroundColor = "#FF943B";
         }
         if (clicks === 4) {
-            newAnswer.style.backgroundColor = "#9D3FE1";
+            newAnswer.style.backgroundColor = "#467CD3";
         }
         if (clicks === 5) {
-            newAnswer.style.backgroundColor = "#68DC49";
+            newAnswer.style.backgroundColor = "#15CF97";
         }
         if (clicks === 6) {
-            newAnswer.style.backgroundColor = "#FFE255";
+            newAnswer.style.backgroundColor = "#9D3FE1";
         }
         if (clicks < 6) {
             wrapper[0].append(newAnswer)
