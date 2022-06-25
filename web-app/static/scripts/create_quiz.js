940f1fbade260541be5f886347933914703aaec3
mpvnlv <v.kruk@innopolis.university>
2022-06-25 19:06:58 +0300

    new scripts

--- web-app/static/scripts/create_quiz.js
+++ web-app/static/scripts/create_quiz.js
@@ -1,38 +1,59 @@
 
 let questions_array=[]
 let answer_array=[]
+let  question_type;
+let time_mode;
 
 document.querySelector("#btn_create").onclick = function(event){
     event.preventDefault();
 
     let quiz_name = $("#quiz_name");  
     let question = $("#question");
+    question_type = $('#select_type').val();
+    time_mode = $("#select_time");
+    console.log( $('#select_type').val())
+    console.log( $('#select_time').val())
 
-    question.val("");
+
+   
+    
  
     $(".Answer_type").each(function(){
-        console.log(this.value)
+
+        answer_json={
+            "text":this.value,
+            "correct_answer": 1,
+        }
+     
         //push answers to array
-        answer_array.push(this.value)
+        answer_array.push(answer_json)
         this.value =""
     })
+    question_json = {
+        "question_type":question_type,
+        "text":question.val(),
+        "time":time_mode.val(),
+        "anwers": answer_array
+    }
+    question.val("");
+    // time_mode.querySelector('[selected]').selected = true
+
+
    
  
 
 document.querySelector("#btn_save").onclick = function(event){
-    question_json = {
-        "anwers": answer_array
-    }
+    
     questions_array.push(question_json)
     //data to request
     data = {
         "name":quiz_name.val(),
         "questions":questions_array,
     }
-    ajax("https://268b-178-205-186-218.ngrok.io/api/user", "POST", response_reg, JSON.stringify(data));
-    function response_reg(dataArr){
+    // ajax("https://268b-178-205-186-218.ngrok.io/api/user", "POST", response_reg, JSON.stringify(data));
+    // function response_reg(dataArr){
        
-    }
+    // }
     console.log(questions_array)
     console.log(data)
 
