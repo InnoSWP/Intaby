3e622305a8b0298869811ed0cf386e9d5e991bf8
mpvnlv <v.kruk@innopolis.university>
2022-07-01 16:15:06 +0300

    USE THIS VERSION FOR FUTURE WORK

---
+++ web-app/static/scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    ($("#connect")).click(function(event){
+        event.preventDefault();
+       if ($("#name").val() == ""){
+        alert("Enter your name");
+       }
+       else{
+        localStorage.setItem("name",$("#name").val())
+        localStorage.setItem("code",$("#code").val())
+        data = {
+            "name": $("#name").val(),
+            "code": $("#code").val()
+        }
+        ajax("", "POST", response_fun, data)
+        function response_fun(data_arr){
+            // console.log(localStorage.getItem("code"))
+            // console.log(12234)
+            window.location.href = "../templates/waiting_hall.html"
+        }
+
+       }
+    })
+});
