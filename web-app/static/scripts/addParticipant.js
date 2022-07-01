89fe6e8487a81821c10991ef1f14460d2c39a250
mpvnlv <v.kruk@innopolis.university>
2022-07-01 16:00:56 +0300

    ---

---
+++ web-app/static/scripts/addParticipant.js
@@ -0,0 +1,17 @@
+window.onload = addParticipant(100)
+
+function addParticipant(number) {
+    var wrapper = document.getElementsByClassName('waiting_hall_wrapper')[0]
+    console.log(wrapper)
+    for (let index = 0; index < number; index++) {
+        var element = createParticipant()
+        wrapper.appendChild(element)
+    }
+}
+
+function createParticipant() {
+   var element = document.createElement('div')
+   element.classList.add("person",  "mt-3", "col-6", "col-md-2" )
+   element.textContent = "Person"
+   return element
+}
